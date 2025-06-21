9dca7fa3ccf967d873335c4d0d356d75deeeacd5
Golu Kumar <optimus4586prime@gmail.com>
2025-06-21 13:01:50 +0000

    Initial commit.

---
+++ src/Mini-task-frontend/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import { Mini_task_backend } from 'declarations/Mini-task-backend';
+
+function App() {
+  const [greeting, setGreeting] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const name = event.target.elements.name.value;
+    Mini_task_backend.greet(name).then((greeting) => {
+      setGreeting(greeting);
+    });
+    return false;
+  }
+
+  return (
+    <main>
+      <img src="/logo2.svg" alt="DFINITY logo" />
+      <br />
+      <br />
+      <form action="#" onSubmit={handleSubmit}>
+        <label htmlFor="name">Enter your name: &nbsp;</label>
+        <input id="name" alt="Name" type="text" />
+        <button type="submit">Click Me!</button>
+      </form>
+      <section id="greeting">{greeting}</section>
+    </main>
+  );
+}
+
+export default App;
